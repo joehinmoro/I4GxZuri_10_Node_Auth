0dbf46010db439a8e8b409b634169b2ab4e10caa
joehinmoro <joehinmoro@gmail.com>
2022-08-20 17:43:45 +0100

    product ctrlr

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,100 @@ const mongoose = require("mongoose");
 const { Product } = require("../models/productModel");
 
 // CONTROL FUNCTIONS
+// INDEX - GET
 const getProducts = async (req, res) => {
-    // query all product
-    const products = await Product.find();
+    try {
+        // query all product
+        const products = await Product.find();
+        // send response
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ error: { code: 500, message: "server error" } });
+    }
 };
+
+// CREATE - POST
+const createProduct = async (req, res) => {
+    try {
+        // destructure from req body
+        const { name, price } = req.body;
+
+        // ensure non-empty fields
+        const empty = [];
+        if (!name) empty.push("name");
+        if (!price) empty.push("price");
+        if (empty.length > 1)
+            return res.status(400).json({ error: "Please fill in all the fields", empty });
+
+        //create
+        const newProduct = await Product.create({ name, price });
+        res.status(200).json(newProduct);
+    } catch (error) {
+        res.status(500).json({ error: { code: 500, message: "server error" } });
+    }
+};
+
+// SHOW - GET
+const getProduct = async (req, res) => {
+    try {
+        // destructure product id from req params
+        const { id } = req.params;
+
+        // verify id is valid
+        if (!mongoose.Types.ObjectId.isValid(id))
+            return res.status(404).json({ error: "invalid id" });
+
+        // find single product
+        const singleProduct = await Product.findById(id);
+        if (!singleProduct) return res.status(404).json({ error: "product not found" });
+
+        // send single product
+        res.status(200).json(singleProduct);
+        //
+    } catch (error) {
+        res.status(500).json({ error: { code: 500, message: "server error" } });
+    }
+};
+
+const updateProduct = async (req, res) => {
+    try {
+        // destructure product id from req params
+        const { id } = req.params;
+
+        // verify id is valid
+        if (!mongoose.Types.ObjectId.isValid(id))
+            return res.status(404).json({ error: "invalid id" });
+
+        // find single product and update
+        const singleProduct = await Product.findByIdAndUpdate(id, { ...req.body });
+        //
+        if (!singleProduct) return res.status(404).json({ error: "product not found" });
+        // send single product
+        res.status(200).json(singleProduct);
+    } catch (error) {
+        res.status(500).json({ error: { code: 500, message: "server error" } });
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    try {
+        // destructure product id from req params
+        const { id } = req.params;
+
+        // verify id is valid
+        if (!mongoose.Types.ObjectId.isValid(id))
+            return res.status(404).json({ error: "invalid id" });
+
+        // find single product and delete
+        const singleProduct = await Product.findByIdAndDelete(id, { ...req.body });
+        //
+        if (!singleProduct) return res.status(404).json({ error: "product not found" });
+        // send single product
+        res.status(200).json(singleProduct);
+    } catch (error) {
+        res.status(500).json({ error: { code: 500, message: "server error" } });
+    }
+};
+
+// EXPORTS
+module.exports = { getProduct, getProducts, updateProduct, createProduct, deleteProduct };
